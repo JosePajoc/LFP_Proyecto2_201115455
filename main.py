4d73c8cb20040a2425fece76f9bfe51801320e25
José Pajoc <computacion.programacion.web@gmail.com>
2021-10-20 20:25:57 -0600

    Mostrar datos del archivo en el editor de texto

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from tkinter import *
 from tkinter import filedialog                      #Módulo para abrir ventana de selección
 from tkinter import messagebox                      #Módulo para cuadros de mensaje
 from tkinter import *                               #Módulo para entorno gráfico
@@ -6,11 +5,12 @@ from tkinter import ttk                             #Módulo para usar comboBoxs
 import re                                           #Módulo de expresiones regulares
 
 #------------------------------------------Global-------------------------------------------------------------
-archivoCargado = False
+archivoCargado = None
+cargarArch = False
 
 #-----------------------------------------Funciones-----------------------------------------------------------
 def abrirArchivo():
-    global archivoCargado
+    global archivoCargado, cargarArch, ventana, txtEditor
     rutaArchivo = filedialog.askopenfilename(title = "Seleccionar archivo XML")
     extension = re.findall('(\.lfp)$', rutaArchivo)                    #<------------ ver extensión valida
     
@@ -21,23 +21,33 @@ def abrirArchivo():
         archivoLFP = archivoCargado.read()
         archivoCargado.close()
         datosDelArchivo = archivoLFP.lower()                               #Cambio a minúsculas
-        print(datosDelArchivo)
+        
         messagebox.showinfo('Información','Cargado con éxito')
-        archivoCargado = True
+        cargarArch = True
+        ventana.title('Consola LFP - ' + rutaArchivo)
+        txtEditor.insert('insert', datosDelArchivo)                         #Mostrar datos del archivo en el editor de texto
     else:
         messagebox.showinfo('Error','El archivo seleccionado no posee extensión \'.pxla\'')
         rutaArchivo = ''
 
 
+def leerCodigo():
+    global txtEditor
+    if cargarArch:
+        codigo = txtEditor.get('1.0', 'end-1c')                                 #Extraer contenido del editor de texto
+        print(codigo)
+    else:
+        messagebox.showwarning('Error', 'No se ha cargado el archivo...')
+
 #----------------------------------Objetos de entorno gráfico Global---------------------------------------------
 ventana = Tk()
 marcoInicial = Frame()
 btnAbrir = Button(marcoInicial, text='Abrir archivo', command=abrirArchivo)
-btnAnalizar = Button(marcoInicial, text='Analizar archivo')
+btnAnalizar = Button(marcoInicial, text='Analizar archivo', command=leerCodigo)
 btnVerReporte = Button(marcoInicial, text='Ver reporte')
 lstSeleccionarReporte = ttk.Combobox(marcoInicial, width=25, state='readonly')      #comboBox
-txtEditor = Text(marcoInicial, bg="#566573", foreground="white")          #Área de texto
-txtConsola = Text(marcoInicial, bg="black", foreground="white", state='disabled')
+txtEditor = Text(marcoInicial, bg="#566573", foreground="white", width=35, height=20)          #Área de texto
+txtConsola = Text(marcoInicial, bg="black", foreground="white", state='disabled', width=35, height=20)
 
 
 #------------------------------------Entorno grafico----------------------------------------------------------
@@ -54,9 +64,7 @@ def ventana_inicial():
     lstSeleccionarReporte['values'] = ['Reporte de Tokens', 'Reporte de Errores', 'árbol de derivación'] #Valores del comboBox
     btnVerReporte.place(x=490, y=20)
     txtEditor.place(x=30, y=100)
-    txtEditor.config(width=35, height=20)
-    txtConsola.place(x=400, y=100)
-    txtConsola.config(width=35, height=20)
+    txtConsola.place(x=400, y=100)  
 
 
 if __name__=='__main__':
